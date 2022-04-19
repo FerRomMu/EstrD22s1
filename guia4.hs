3ab7d33a1f6363504d69a9bf8eeed6c86e320b32
Fernando Romero <fer.rom.mu@gmail.com>
2022-04-18 23:25:07 -0300

    Empece la parte de tesoros.

--- guia4.hs
+++ guia4.hs
@@ -52,3 +52,95 @@ pizza = Capa (Aceitunas 8) (Capa Salsa (Capa Queso (Capa Jamon Prepizza)))
 pizza2 = Prepizza
 pizza3 = Capa Salsa (Capa Queso (Capa Salsa Prepizza))
 
+--Mapa de tesoros
+
+data Dir = Izq | Der
+data Objeto = Tesoro | Chatarra
+data Cofre = Cofre [Objeto]
+data Mapa = Fin Cofre
+          | Bifurcacion Cofre Mapa Mapa
+
+map = Bifurcacion (Cofre [Chatarra]) 
+        (Bifurcacion (Cofre [Chatarra, Chatarra, Chatarra])
+          (Bifurcacion (Cofre []) (Fin (Cofre [])) (Fin (Cofre [])))
+          (Bifurcacion (Cofre [Chatarra, Tesoro, Chatarra]) (Fin (Cofre [])) (Fin (Cofre [])))
+        )
+        (Fin (Cofre []))
+--1.
+hayTesoro :: Mapa -> Bool
+--Indica si hay un tesoro en alguna parte del mapa
+hayTesoro (Fin c) = hayTesoroEnCofre c
+hayTesoro (Bifurcacion c m1 m2) = (hayTesoroEnCofre c) 
+                                || (hayTesoro m1) 
+                                || (hayTesoro m2)
+
+hayTesoroEnCofre :: Cofre -> Bool
+hayTesoroEnCofre (Cofre os) = hayTesoroEntre os
+
+hayTesoroEntre :: [Objeto] -> Bool
+hayTesoroEntre [] = False
+hayTesoroEntre (o:os) = (esTesoro o) || (hayTesoroEntre os)
+
+esTesoro :: Objeto -> Bool
+esTesoro Tesoro = True
+esTesoro _ = False
+
+--2.
+hayTesoroEn :: [Dir] -> Mapa -> Bool
+hayTesoroEn d m = hayTesoroEnCofre(obtenerCofreSiHayEn d m)
+
+obtenerCofreSiHayEn :: [Dir] -> Mapa -> Cofre
+--Aclaracion: Funcion absoluta, si no hay camino suficiente en el mapa devuelve un
+--cofre vacio.
+obtenerCofreSiHayEn [] m = cofreEn m
+--Una mejor solucion seria usando el tipo just para evitar el cofre vacio
+obtenerCofreSiHayEn (x:xs) (Fin c) = Cofre [] --Caso borde, no hay camino suficiente
+obtenerCofreSiHayEn (x:xs) (Bifurcacion c m1 m2) = 
+                          if(esIzq x) 
+                            then obtenerCofreSiHayEn xs m1 
+                            else obtenerCofreSiHayEn xs m2
+
+esIzq :: Dir -> Bool
+esIzq Izq = True
+esIzq _ = False
+
+cofreEn :: Mapa -> Cofre
+cofreEn (Fin c) = c
+cofreEn (Bifurcacion c m1 m2) = c
+
+--Mismo ejercicio pero de manera parcial
+hayTesoroEnParcial :: [Dir] -> Mapa -> Bool
+--precon: Hay camino en el mapa para llegar con las direcciones dadas.
+hayTesoroEnParcial d m = hayTesoroEnCofre(cofreEn(avanzarHasta d m))
+
+avanzarHasta :: [Dir] -> Mapa -> Mapa
+--precon: Hay camino en el mapa para llegar con las direcciones dadas.
+avanzarHasta [] m = m
+avanzarHasta (x:xs) (Fin c) = error "no cumplio precondición"
+avanzarHasta (x:xs) (Bifurcacion c m1 m2) =
+                       if(esIzq x) 
+                        then avanzarHasta xs m1
+                        else avanzarHasta xs m2
+
+--3.
+caminoAlTesoro :: Mapa -> [Dir]
+--precon: Existe tesoro y es unico.
+caminoAlTesoro m = snd (caminoAlTesoroConDir m)
+
+--Entiendo que es choclodigo pero no se me ocurre otra forma
+--de poder verificar si hay tesoro en esa rama para irla guardando
+--de manera recursiva.
+caminoAlTesoroConDir :: Mapa -> (Bool, [Dir])
+caminoAlTesoroConDir (Fin c) = (hayTesoroEnCofre c, [])
+caminoAlTesoroConDir (Bifurcacion c m1 m2) =
+  let 
+    (bool1, dirs1) = caminoAlTesoroConDir m1
+    (bool2, dirs2) = caminoAlTesoroConDir m2
+  in
+  --mira si ya alguna rama tiene el camino al tesoro
+    if (bool1)
+    then (bool1, Izq : dirs1)
+    else if (bool2)
+      then (bool2, Der : dirs2)
+      --En caso negativo, se fija si este nodo tiene tesoro y genera la base.
+      else (hayTesoroEnCofre c, [])
